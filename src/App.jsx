c3b4b5626b8bea2acf8d8c9f196499fdc1ec8638
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-07-27 06:00:07 -0400

    deployed project v.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1>Tru-Salomon</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hello Reader, Here are a few info on my skills and experience.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <img src="https://www.breakthrough-pt.com/wp-content/uploads/2014/11/female-default-profile-photo.png" alt="" />
+
+      <div>
+        <h3>Here are some of my Skills!</h3>
+        <ul>
+          <h3>Languages</h3>
+          <li>JavaScript</li>
+          <li>SQL</li>
+          <li>HTML5</li>
+          <li>CSS3</li>
+        </ul>
+
+        <ol>
+          <h3>Libraries</h3>
+          <li>React</li>
+          <li>Node</li>
+
+        </ol>
+      </div>
+
+      <div>
+        <h3>Hardest Task Accomplished So Far</h3>
+        <p>
+          Creating a CRUD application using inquirer.
+        </p>
+        <p>I wanted a more unique approach where the user to be asked to put certain input depending on what they wanted to
+          achieve. <br />
+          As a result the project would be more beginner friendly.
+        </p>
+      </div>
+
+      <h2>Contact Me</h2>
+      <div>
+        Email me at:
+        <a href="mailto:danielbalthazar@pursuit.org" target="_top">danielbalthazar@pursuit.org</a>
+      </div>
+
+      <ul>
+        <li><a href="https://github.com/tru-salomon">github</a></li>
+        <li><a href="linkedin.com/danielsalomonbalthazar">LinkedIn</a></li>
+        <li><a href="https://replit.com/@DanielBalthaza1">Replit</a></li>
+      </ul>
     </>
   )
 }
